ca5c4ef88175651ba0ee17ac642528f11dfc4ff4
Putra Utama <putrautama30@gmail.com>
2019-07-16 12:24:53 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter GoogleMaps Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:HomePage(),
+    );
+  }
+}
+
+
